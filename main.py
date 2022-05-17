0346376d88ee76bc264d181d34c6463cfb0addfb
bobylev <alexey.bobylev@flant.com>
2022-05-18 00:01:42 +0300

    Изменён формат хранения данных Изменён набор глобальных переменных (выбран окончательный вариант) Добавлены описания функций Добавлен файл сохранений

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 # imports
-import configparser
+from configparser import ConfigParser
 
 # global variables
 
 FIELD = [[' '] * 3 for _ in range(3)]
+PLAYERS = {}
+SAVES = {}
 
 
 # functions
-# описания функций обязательны!
-def field():
+def field() -> None:
+    """Вывод на экран игрового поля с ходами"""
     # хорошее поле получилось
     global FIELD
     frame_h = "+———+———+———+"
@@ -16,40 +18,53 @@ def field():
         print(frame_h)
         print(f"| {row[0]} | {row[1]} | {row[2]} |")
     print(frame_h)
-    pass
 
 
-# ini file reading
-
-# описания функций обязательны!
-def print_menu(menu="main"):
+def print_menu(menu="main") -> None:
+    """Вывод на экран игровых меню"""
     if menu == "main":
         print("1 - один игрок")
         print("2 - два игрока")
 
 
-# описания функций обязательны!
-def load(player_name) -> dict:
-    cp = configparser.ConfigParser()
-    res = {player_name: {}}
-    try:
-        cp.read("data.ini")
-        for key in cp[player_name]:
-            res[player_name][key] = cp[player_name][key]
-    except:
-        res = {}
-    return res
+def show_help() -> None:
+    """Вывод на экран справочной информации"""
+    # TODO реализовать функцию
+    pass
 
 
-# описания функций обязательны!
-def save(player_name: str, player_stats: dict):
-    cp = configparser.ConfigParser()
-    cp[player_name] = player_stats
-    with open("data.ini", "a") as f:
-        cp.write(f)
+def read() -> bool:
+    """Загрузка сохранённых данных из файла в глобальные переменные"""
+    global PLAYERS, SAVES
+    config = ConfigParser()
+    if config.read("data.ini", encoding="utf-8"):
+        PLAYERS = {name.title(): [int(n) for n in score.split(',')]
+                   for name, score in config["Scores"].items()}
+        SAVES = {tuple(name.split(';')):
+                     [[' ' if c == '-' else c for c in field[i:i + 3]]]
+                 for i in range(0, 9, 3) for name, field in config["Saves"].items()}
+        return True if config["General"]["first"] == "yes" else False
+    else:
+        raise FileNotFoundError
 
 
-# begin
+def save() -> None:
+    """Сохранение данных из глобальных переменных в файл"""
+    global PLAYERS, SAVES
+    config = ConfigParser()
+    config["Scores"] = {name: ','.join([str(x) for x in score]) for name, score in PLAYERS.items()}
+    config["Saves"] = {';'.join(name):
+                           ''.join(['-' if cell == ' ' else cell for row in field for cell in row])
+                       for name, field in SAVES.items()}
+    config["General"]["first"] = "no"
+    with open("data.ini", 'w', encoding="utf-8") as f:
+        config.write(f)
+
+
+if read():
+    show_help()
+
+# Основной цикл игры
 while True:
     # я бы начал со справки: описать и себе и пользователю: что ты пользователю показать и что, от него получить
     print_menu()
@@ -58,20 +73,3 @@ while True:
     if command in ('quit', 'выход'):
         # Обработка завершения работы приложения
         break
-    # используй комментарии! описывай, что делаешь
-    elif command == "1":
-        player_name = input("Ввведите имя игрока: ")
-        if not load(player_name):
-            save(player_name, {"wins": 0, "loses": 0, "draws": 0})
-        else:
-            player_stats = load(player_name)
-            print(f"Статистика игрока {player_name}:\n"
-                  f"Побед - {player_stats[player_name]['wins']}\n"
-                  f"Поражений - {player_stats[player_name]['loses']}\n"
-                  f"Ничьих - {player_stats[player_name]['draws']}")
-
-# configparser работаем со словарём
-
-
-# commit messages должны быть более информативные
-# также, лучше больше коммитов, чем меньше
